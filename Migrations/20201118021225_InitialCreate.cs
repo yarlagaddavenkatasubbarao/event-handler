4468a0a4d3be99a52bf8f4ba9cc5495ce6297fb3
yarlagaddavenkatasubbarao <yarlagadda.venkat.chinni@gmail.com>
2020-12-04 12:09:41 -0600

    Add files via upload

---
+++ Migrations/20201118021225_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ISC567Web.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CompanyName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventCategories",
+                columns: table => new
+                {
+                    EventCategoryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventCategoryLogo = table.Column<string>(nullable: true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventCategories", x => x.EventCategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advertisers",
+                columns: table => new
+                {
+                    AdvertiserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    CompanyID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advertisers", x => x.AdvertiserID);
+                    table.ForeignKey(
+                        name: "FK_Advertisers_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventName = table.Column<string>(nullable: true),
+                    EventLocation = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    EventLogo = table.Column<string>(nullable: true),
+                    Promotional = table.Column<bool>(nullable: false),
+                    EventCategoryID = table.Column<int>(nullable: true),
+                    AdvertiserID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventID);
+                    table.ForeignKey(
+                        name: "FK_Events_Advertisers_AdvertiserID",
+                        column: x => x.AdvertiserID,
+                        principalTable: "Advertisers",
+                        principalColumn: "AdvertiserID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Events_EventCategories_EventCategoryID",
+                        column: x => x.EventCategoryID,
+                        principalTable: "EventCategories",
+                        principalColumn: "EventCategoryID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisers_CompanyID",
+                table: "Advertisers",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_AdvertiserID",
+                table: "Events",
+                column: "AdvertiserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_EventCategoryID",
+                table: "Events",
+                column: "EventCategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Advertisers");
+
+            migrationBuilder.DropTable(
+                name: "EventCategories");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
